4e4596bfad77d50753e3540e52779cbf5443f90a
mtmoreira <matheus@sundial.chronostech.com>
2020-01-13 17:46:49 -0800

    Submitting updates in dir and text file objs

---
+++ test/fileTypes/test_DirFileObj.py
@@ -0,0 +1,260 @@
+## \file test_DirFileObj.py
+#  \brief Testcases for DirFileObj class
+#
+# Revision | Author            | Date     | Comment
+#:---------|:------------------|:---------|:----------------
+# 1.0      | Matheus T. M.     | 01/12/20 | Initial version
+#
+import unittest
+import os
+import sys
+import shutil
+sys.path.append(os.path.realpath("../src/fileTypes"))
+from DirFileObj import DirFileObj
+
+class DirFileObjTest(unittest.TestCase):
+
+	def setUp(self):
+		self.rootFolder = "./"
+		self.testFolderName = ".DirFileObjTest"
+		self.testFolder = os.path.realpath(self.rootFolder+self.testFolderName)
+		# Create it
+		try:
+			os.mkdir(self.testFolder)
+		except Exception as e:
+			raise RuntimeError("Error trying to create directory %s: %s" % (self.testFolder,str(e)))
+		# # Sample file to write
+		# self.writeFilePath = self.testFolder+"/"+"file0.txt"
+		# # Sample file to read
+		# self.readFilePath = os.path.realpath("./fileTypes/dirExample/file0.txt")
+		# # File to read and edit
+		# self.subReadFilePath = os.path.realpath("./fileTypes/dirExample/fileSub.txt")
+
+	def tearDown(self):
+		shutil.rmtree(self.testFolder)
+
+	## Building a DirFileObj
+	def test_emptyConstructor(self):
+		with self.assertRaises(RuntimeError):
+			DirFileObj()
+
+	def test_constructorPathTypeError(self):
+		with self.assertRaises(TypeError):
+			DirFileObj(path=0)
+
+	def test_constructorPath(self):
+		dirPath = os.path.realpath(self.rootFolder+"fileTypes/dirExample")
+		d = DirFileObj(path=dirPath)
+		self.assertEqual(d.getDirList(),["subdirExample"])
+		self.assertEqual(d.getFileList(),["file0.txt"])
+		f = d.getFile("file0.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "With\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "5 lines of text\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+		d = d.getDir("subdirExample")
+		self.assertEqual(d.getDirList(),[])
+		self.assertEqual(d.getFileList(),["fileSub.txt"])
+		f = d.getFile("fileSub.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp For Regexp For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "MyRegexp\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+
+	def test_constructorPathExtraBackSlack(self):
+		dirName = "dirExample/"
+		dirPath = os.path.realpath(self.rootFolder+"fileTypes/"+dirName)
+		d = DirFileObj(path=dirPath)
+		self.assertEqual(d.name,"dirExample")
+		self.assertEqual(d.getDirList(),["subdirExample"])
+		self.assertEqual(d.getFileList(),["file0.txt"])
+		f = d.getFile("file0.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "With\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "5 lines of text\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+		d = d.getDir("subdirExample")
+		self.assertEqual(d.getDirList(),[])
+		self.assertEqual(d.getFileList(),["fileSub.txt"])
+		f = d.getFile("fileSub.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp For Regexp For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "MyRegexp\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+
+	def test_constructorPathExtraExtraBackSlack(self):
+		dirName = "dirExample////"
+		dirPath = os.path.realpath(self.rootFolder+"///fileTypes//"+dirName)
+		d = DirFileObj(path=dirPath)
+		self.assertEqual(d.name,"dirExample")
+		self.assertEqual(d.getDirList(),["subdirExample"])
+		self.assertEqual(d.getFileList(),["file0.txt"])
+		f = d.getFile("file0.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "With\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "5 lines of text\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+		d = d.getDir("subdirExample")
+		self.assertEqual(d.getDirList(),[])
+		self.assertEqual(d.getFileList(),["fileSub.txt"])
+		f = d.getFile("fileSub.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp For Regexp For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "MyRegexp\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+
+	def test_constructorWithFatherNameTypeError(self):
+		dirName = "tmpDir"
+		rootD = DirFileObj(path=self.testFolder)
+		with self.assertRaises(TypeError):
+			newD0 = DirFileObj(name=0, father=rootD)
+
+	def test_constructorWithFatherFatherTypeError(self):
+		dirName = "tmpDir"
+		rootD = DirFileObj(path=self.testFolder)
+		with self.assertRaises(TypeError):
+			newD0 = DirFileObj(name=dirName, father=0)
+
+	def test_constructorWithFatherFileAsFather(self):
+		dirName = "tmpDir"
+		dirPath = os.path.realpath(self.rootFolder+"fileTypes/dirExample")
+		d = DirFileObj(path=dirPath)
+		f = d.getFile("file0.txt")
+		with self.assertRaises(RuntimeError):
+			newD = DirFileObj(name=dirName, father=f)
+
+	def test_constructorWithFatherAlreadyHasDir(self):
+		dirName = "tmpDir"
+		rootD = DirFileObj(path=self.testFolder)
+		newD0 = DirFileObj(name=dirName, father=rootD)
+		with self.assertRaises(RuntimeError):
+			newD1 = DirFileObj(name=dirName, father=rootD)
+
+	def test_constructorWithFather(self):
+		dirName = "tmpDir"
+		rootD = DirFileObj(path=self.testFolder)
+		self.assertEqual(rootD.getDirList(),[])
+		self.assertEqual(rootD.getFileList(),[])
+		newD0 = DirFileObj(name=dirName, father=rootD)
+		self.assertEqual(rootD.getDirList(),[dirName])
+		self.assertEqual(rootD.getFileList(),[])
+		self.assertEqual(newD0.getDirList(),[])
+		self.assertEqual(newD0.getFileList(),[])
+		newD1 = DirFileObj(name=dirName, father=newD0)
+		self.assertEqual(rootD.getDirList(),[dirName])
+		self.assertEqual(rootD.getFileList(),[])
+		self.assertEqual(newD0.getDirList(),[dirName])
+		self.assertEqual(newD0.getFileList(),[])
+		self.assertEqual(newD1.getDirList(),[])
+		self.assertEqual(newD1.getFileList(),[])
+		newD0.write()
+		self.assertEqual(os.listdir(self.testFolder),[dirName])
+		self.assertEqual(os.listdir(self.testFolder+"/"+dirName),[dirName])
+
+	# Getting directories
+	def test_getDirTypeError(self):
+		dirName = "tmpDir"
+		rootD = DirFileObj(path=self.testFolder)
+		newD0 = DirFileObj(name=dirName, father=rootD)
+		with self.assertRaises(TypeError):
+			rootD.getDir(0)
+
+	def test_getDirInvalid(self):
+		dirName = "tmpDir"
+		rootD = DirFileObj(path=self.testFolder)
+		newD0 = DirFileObj(name=dirName, father=rootD)
+		with self.assertRaises(ValueError):
+			rootD.getDir("invalid")
+
+	def test_getDir(self):
+		dirName = "tmpDir"
+		rootD = DirFileObj(path=self.testFolder)
+		newD0 = DirFileObj(name=dirName, father=rootD)
+		newD1 = DirFileObj(name=dirName, father=newD0)
+		d = rootD.getDir(dirName)
+		self.assertEqual(d.name,dirName)
+		self.assertEqual(d.getDirList(),[dirName])
+		self.assertEqual(d.getFileList(),[])
+		self.assertEqual(d.father.name,self.testFolderName)
+
+	# Getting files
+	def test_getFileTypeError(self):
+		dirName = "tmpDir"
+		dirPath = os.path.realpath(self.rootFolder+"fileTypes/dirExample")
+		d = DirFileObj(path=dirPath)
+		with self.assertRaises(TypeError):
+			d.getFile(0)
+
+	def test_getFileInvalid(self):
+		dirName = "tmpDir"
+		dirPath = os.path.realpath(self.rootFolder+"fileTypes/dirExample")
+		d = DirFileObj(path=dirPath)
+		with self.assertRaises(ValueError):
+			d.getFile("invalid")
+
+	def test_getFile(self):
+		dirName = "tmpDir"
+		dirPath = os.path.realpath(self.rootFolder+"fileTypes/dirExample")
+		d = DirFileObj(path=dirPath)
+		f = d.getFile("file0.txt")
+		self.assertEqual(f.name,"file0.txt")
+
+	# Copying directories
+	def test_copy(self):
+		dirPath = os.path.realpath(self.rootFolder+"fileTypes/dirExample")
+		rootD = DirFileObj(path=self.testFolder)
+		d = DirFileObj(path=dirPath)
+		# Create a copy to test folder
+		d.copy(name="objCopy", father=rootD)
+		# Check contents
+		self.assertEqual(rootD.getDirList(),["objCopy"])
+		d = rootD.getDir("objCopy")
+		self.assertEqual(d.getFileList(),["file0.txt"])
+		f = d.getFile("file0.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "With\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "5 lines of text\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+		d = d.getDir("subdirExample")
+		self.assertEqual(d.getDirList(),[])
+		self.assertEqual(d.getFileList(),["fileSub.txt"])
+		f = d.getFile("fileSub.txt")
+		expectedFileContent  = "Sample File\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp For Regexp For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "For Regexp\n"
+		expectedFileContent += "\n"
+		expectedFileContent += "MyRegexp\n"
+		expectedFileContent += ""
+		self.assertEqual(f.getStr(),expectedFileContent)
+		# Check actual folder in filesys
+		self.assertEqual(os.listdir(self.testFolder),[])
+		rootD.write()
--- test/fileTypes/test_TextFileObj.py
+++ test/fileTypes/test_TextFileObj.py
@@ -21,8 +21,6 @@ class TextFileObjTest(unittest.TestCase):
 			os.mkdir(self.testFolder)
 		except Exception as e:
 			raise RuntimeError("Error trying to create directory %s: %s" % (self.testFolder,str(e)))
-		self.operW    = TextFileObj.operationLabelWrite
-		self.operR    = TextFileObj.operationLabelRead
 		# Sample file to write
 		self.writeFilePath = self.testFolder+"/"+"file0.txt"
 		# Sample file to read
@@ -34,120 +32,101 @@ class TextFileObjTest(unittest.TestCase):
 		shutil.rmtree(self.testFolder)
 
 	## Building a TextFileObj
-	def test_constructorMissingPath(self):
-		with self.assertRaises(TypeError):
-			TextFileObj(oper=self.operW)
-		self.assertEqual(os.path.exists(self.writeFilePath),False)
-
-	def test_constructorMissingOper(self):
-		with self.assertRaises(TypeError):
-			TextFileObj(path=self.writeFilePath)
+	def test_emptyConstructor(self):
+		with self.assertRaises(RuntimeError):
+			TextFileObj()
 		self.assertEqual(os.path.exists(self.writeFilePath),False)
 
 	def test_constructorPathTypeError(self):
 		with self.assertRaises(TypeError):
-			TextFileObj(path=0,oper=self.operW)
+			TextFileObj(path=0)
 		self.assertEqual(os.path.exists(self.writeFilePath),False)
 
-	def test_constructorPathExists(self):
-		with self.assertRaises(RuntimeError):
-			TextFileObj(path=self.testFolder,oper=self.operW)
-		self.assertEqual(os.path.exists(self.writeFilePath),False)
+	# def test_constructorPath(self):
+	# 	TextFileObj(path=0)
+	# 	self.assertEqual(os.path.exists(self.writeFilePath),False)
 
-	def test_constructorOperTypeError(self):
-		with self.assertRaises(TypeError):
-			TextFileObj(path=self.writeFilePath,oper=0)
-		self.assertEqual(os.path.exists(self.writeFilePath),False)
+	# def test_constructorNameFatherMissingName(self):
+	# 	with self.assertRaises(TypeError):
+	# 		TextFileObj(name="", father=)
+	# 	self.assertEqual(os.path.exists(self.writeFilePath),False)
 
-	def test_constructorOperValueError(self):
-		with self.assertRaises(ValueError):
-			TextFileObj(path=self.writeFilePath,oper="invalid")
-		self.assertEqual(os.path.exists(self.writeFilePath),False)
+# 	def test_constructorWrite(self):
+# 		tf = TextFileObj(path=self.writeFilePath,oper=self.operW)
+# 		self.assertEqual(os.path.exists(self.writeFilePath),True)
+# 		self.assertEqual(tf.path, self.writeFilePath)
+# 		self.assertEqual(tf.isLocked(), False)
+# 		self.assertEqual(tf.lineList, [])
+# 		self.assertEqual(tf.lineCount, 0)
 
-	def test_constructorWriteExistingFile(self):
-		with self.assertRaises(RuntimeError):
-			TextFileObj(path=self.readFilePath,oper=self.operW)
+# 	def test_constructorRead(self):
+# 		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
+# 		self.assertEqual(tf.path, self.readFilePath)
+# 		self.assertEqual(tf.isLocked(), True)
+# 		self.assertEqual(tf.lineList, ['Sample File', '', 'With', '', '5 lines of text'])
+# 		self.assertEqual(tf.lineCount, 5)
 
-	def test_constructorReadNonExistingFile(self):
-		with self.assertRaises(RuntimeError):
-			TextFileObj(path=self.writeFilePath,oper=self.operR)
+# 	## Setting a path
+# 	def test_setPathTypeError(self):
+# 		tf = TextFileObj(path=self.writeFilePath,oper=self.operW)
+# 		with self.assertRaises(TypeError):
+# 			tf.path = 0
 
-	def test_constructorWrite(self):
-		tf = TextFileObj(path=self.writeFilePath,oper=self.operW)
-		self.assertEqual(os.path.exists(self.writeFilePath),True)
-		self.assertEqual(tf.path, self.writeFilePath)
-		self.assertEqual(tf.isLocked(), False)
-		self.assertEqual(tf.lineList, [])
-		self.assertEqual(tf.lineCount, 0)
+# 	def test_setPathLocked(self):
+# 		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
+# 		with self.assertRaises(RuntimeError):
+# 			tf.path = 0
 
-	def test_constructorRead(self):
-		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
-		self.assertEqual(tf.path, self.readFilePath)
-		self.assertEqual(tf.isLocked(), True)
-		self.assertEqual(tf.lineList, ['Sample File', '', 'With', '', '5 lines of text'])
-		self.assertEqual(tf.lineCount, 5)
+# 	def test_setPath(self):
+# 		tf = TextFileObj(path=self.writeFilePath,oper=self.operW)
+# 		tf.path = "new_path"
+# 		self.assertEqual(tf.path, "new_path")
 
-	## Setting a path
-	def test_setPathTypeError(self):
-		tf = TextFileObj(path=self.writeFilePath,oper=self.operW)
-		with self.assertRaises(TypeError):
-			tf.path = 0
+# # TODO: Add write/read checks
 
-	def test_setPathLocked(self):
-		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
-		with self.assertRaises(RuntimeError):
-			tf.path = 0
+# 	## Copying a TextFileObj
+# 	def test_copyTypeError(self):
+# 		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
+# 		with self.assertRaises(TypeError):
+# 			tf.copyFile(newPath=0)
 
-	def test_setPath(self):
-		tf = TextFileObj(path=self.writeFilePath,oper=self.operW)
-		tf.path = "new_path"
-		self.assertEqual(tf.path, "new_path")
+# 	def test_copyFile(self):
+# 		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
+# 		newTf = tf.copyFile(newPath=self.writeFilePath)
+# 		self.assertEqual(newTf.isLocked(), False)
+# 		self.assertEqual(newTf.lineList, ['Sample File', '', 'With', '', '5 lines of text'])
+# 		self.assertEqual(newTf.lineCount, 5)
+# 		newTf.lock()
+# 		self.assertEqual(newTf.isLocked(), True)
+# 		tf = TextFileObj(path=self.writeFilePath,oper=self.operR)
+# 		self.assertEqual(tf.path, self.writeFilePath)
+# 		self.assertEqual(tf.isLocked(), True)
+# 		self.assertEqual(tf.lineList, ['Sample File', '', 'With', '', '5 lines of text'])
+# 		self.assertEqual(tf.lineCount, 5)
 
-# TODO: Add write/read checks
+# 	## Substituting strings in lines
+# 	def test_strSubFindStrTypeError(self):
+# 		tf = TextFileObj(path=self.subReadFilePath,oper=self.operR)
+# 		with self.assertRaises(TypeError):
+# 			tf.strSub(findStr=0,replaceStr="Example")
 
-	## Copying a TextFileObj
-	def test_copyTypeError(self):
-		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
-		with self.assertRaises(TypeError):
-			tf.copyFile(newPath=0)
+# 	def test_strSubReplaceStrTypeError(self):
+# 		tf = TextFileObj(path=self.subReadFilePath,oper=self.operR)
+# 		with self.assertRaises(TypeError):
+# 			tf.strSub(findStr="Sample",replaceStr=0)
 
-	def test_copyFile(self):
-		tf = TextFileObj(path=self.readFilePath,oper=self.operR)
-		newTf = tf.copyFile(newPath=self.writeFilePath)
-		self.assertEqual(newTf.isLocked(), False)
-		self.assertEqual(newTf.lineList, ['Sample File', '', 'With', '', '5 lines of text'])
-		self.assertEqual(newTf.lineCount, 5)
-		newTf.lock()
-		self.assertEqual(newTf.isLocked(), True)
-		tf = TextFileObj(path=self.writeFilePath,oper=self.operR)
-		self.assertEqual(tf.path, self.writeFilePath)
-		self.assertEqual(tf.isLocked(), True)
-		self.assertEqual(tf.lineList, ['Sample File', '', 'With', '', '5 lines of text'])
-		self.assertEqual(tf.lineCount, 5)
-
-	## Substituting strings in lines
-	def test_strSubFindStrTypeError(self):
-		tf = TextFileObj(path=self.subReadFilePath,oper=self.operR)
-		with self.assertRaises(TypeError):
-			tf.strSub(findStr=0,replaceStr="Example")
-
-	def test_strSubReplaceStrTypeError(self):
-		tf = TextFileObj(path=self.subReadFilePath,oper=self.operR)
-		with self.assertRaises(TypeError):
-			tf.strSub(findStr="Sample",replaceStr=0)
-
-	def test_strSub(self):
-		tf = TextFileObj(path=self.subReadFilePath,oper=self.operR)
-		newTf = tf.copyFile(newPath=self.writeFilePath)
-		newTf.strSub(findStr="Sample",replaceStr="Example")
-		self.assertEqual(newTf.lineList, ['Example File', '', 'For Regexp For Regexp For Regexp', '', 'For Regexp', '', 'MyRegexp'])
-		self.assertEqual(newTf.lineCount, 7)
-		tf = TextFileObj(path=self.writeFilePath,oper=self.operR)
-		self.assertEqual(tf.lineList, ['Example File', '', 'For Regexp For Regexp For Regexp', '', 'For Regexp', '', 'MyRegexp'])
-		self.assertEqual(tf.lineCount, 7)
-		newTf.strSub(findStr="For Regexp",replaceStr="0")
-		self.assertEqual(newTf.lineList, ['Example File', '', '0 0 0', '', '0', '', 'MyRegexp'])
-		self.assertEqual(newTf.lineCount, 7)
-		tf = TextFileObj(path=self.writeFilePath,oper=self.operR)
-		self.assertEqual(tf.lineList, ['Example File', '', '0 0 0', '', '0', '', 'MyRegexp'])
-		self.assertEqual(tf.lineCount, 7)
+# 	def test_strSub(self):
+# 		tf = TextFileObj(path=self.subReadFilePath,oper=self.operR)
+# 		newTf = tf.copyFile(newPath=self.writeFilePath)
+# 		newTf.strSub(findStr="Sample",replaceStr="Example")
+# 		self.assertEqual(newTf.lineList, ['Example File', '', 'For Regexp For Regexp For Regexp', '', 'For Regexp', '', 'MyRegexp'])
+# 		self.assertEqual(newTf.lineCount, 7)
+# 		tf = TextFileObj(path=self.writeFilePath,oper=self.operR)
+# 		self.assertEqual(tf.lineList, ['Example File', '', 'For Regexp For Regexp For Regexp', '', 'For Regexp', '', 'MyRegexp'])
+# 		self.assertEqual(tf.lineCount, 7)
+# 		newTf.strSub(findStr="For Regexp",replaceStr="0")
+# 		self.assertEqual(newTf.lineList, ['Example File', '', '0 0 0', '', '0', '', 'MyRegexp'])
+# 		self.assertEqual(newTf.lineCount, 7)
+# 		tf = TextFileObj(path=self.writeFilePath,oper=self.operR)
+# 		self.assertEqual(tf.lineList, ['Example File', '', '0 0 0', '', '0', '', 'MyRegexp'])
+# 		self.assertEqual(tf.lineCount, 7)
